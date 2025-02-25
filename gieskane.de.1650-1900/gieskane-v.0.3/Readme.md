7620bb7945d47e39b8522ce39de7f00c5901255f
Christian Chiarcos <christian.chiarcos@uni-a.de>
2025-02-25 11:09:12 +0100

    GiesKaNe CSV dump CoNLL exporter

--- gieskane.de.1650-1900/gieskane-v.0.3/Readme.md
+++ gieskane.de.1650-1900/gieskane-v.0.3/Readme.md
@@ -2,6 +2,16 @@
 
 Modern High German, 1650-1900
 
+Unfortunately, the corpus is **not available as a dataset**. We can retrieve parts of the annotation from a corpus management system, but this does not include annotations for syntax. In the context of GermHist, the corpus is thus currently unusable.
+
+For building the corpus and storing it in `annis-export.conll`, run
+
+    $> make
+
+The corpus is built over DTA, the added value in comparison to DTA is marginal. Apparently, the morphosyntactic annotations have been manually corrected.
+
+## Corpus information
+
 - see https://www.uni-giessen.de/de/fbz/gieskane/korpus/zugang
 - corpus composition: https://www.uni-giessen.de/de/fbz/gieskane/korpus/korpustexte
   - source urls are provided and can be used for automated alignment
@@ -11,5 +21,8 @@ Modern High German, 1650-1900
    - **FAILED** search for "orthsatz", then "export" and export via GridExporter (set preceding and following context to 0) results in file with sentence numbers, only
    - **FAILED** search for "tok", GridExporter, +-1 token, results in list of numbers (no annotations whatsoever)
    - **FAILED** search for "tok", CSVExporter, nur plain text export
-   - search for `node & tok & #1 _i_ #2`, CSVExporter. we loose the function labels ... and this is a **huge** dump, from which most parts of the annotation can be recovered
+   - search for `node & tok & #1 _i_ #2`, CSVExporter. we loose the function labels ... and this is a **huge** dump (650MB), from which word-level annotation can be recovered
 - note that this export is limited to token-level annotations, so we loose all span annotations
+
+TODO:
+- check whether `node & tok & #2 _i_ #1` retrieves a larger dump. Then, the order of `_i_` was incorrectly documented.
--- gieskane.de.1650-1900/gieskane-v.0.3/jsonl2conll.py
+++ gieskane.de.1650-1900/gieskane-v.0.3/jsonl2conll.py
@@ -1,7 +1,9 @@
-import sys,os,re,json,argparse
+import sys,os,re,json,argparse,traceback
 args=argparse.ArgumentParser(description="read JSON-L from stdin, write CoNLL; note that we don't reorder, so we expect things to be presented in reading order")
 args.add_argument("config", type=str, help="JSONL configuration for column export, note that we only export features declared there, and that multiple features can be mapped to the same column, |-concatenated")
 args.add_argument("files", nargs="*",type=str, help="JSON-L files to read from, alternatively, from stdin", default=[])
+args.add_argument("-s", "--split_sentence_at", type=str, nargs="*", help="one or more regular expressions that define sentence splits; for one key and one value, separated by '=', e.g., S=$. If more than one condition are specified, we treat them as alternative criteria (disjunction)", default=[])
+args.add_argument("-n", "--add_numerical_IDs", action="store_true", help="add CoNLL-style word numbers as first column; note that this is extrapolated from sentence splits and incoming lines. this may or may not match the original corpus IDs")
 args=args.parse_args()
 
 config=[]
@@ -16,7 +18,11 @@ if len(args.files)==0:
 	args.files=[sys.stdin]
 	sys.stderr.write("reading from stdin\n")
 
-print("# "+"\t".join([c["label"] for c in config ]))
+header=[c["label"] for c in config ]
+if args.add_numerical_IDs:
+	header=["ID"]+header
+
+print("# "+"\t".join(header))
 print()
 
 for file in args.files:
@@ -25,11 +31,22 @@ for file in args.files:
 		print(f"# file: {file}\n")
 		file=open(file,"rt",errors="ignore")
 
+	word_nr=0
+
 	for line in file:
 		line=line.strip()
 		if len(line)>0:
-			anno=json.loads(line)
+			try:
+				anno=json.loads(line)
+			except json.decoder.JSONDecodeError:
+				traceback.print_exc()
+				print(line)
+				continue
+
 			result=[]
+			if args.add_numerical_IDs:
+				word_nr+=1
+				result.append(str(word_nr))
 			for col in config:
 				add=[]
 				for key in col["keys"]:
@@ -42,5 +59,26 @@ for file in args.files:
 					add="_"
 				result.append(add)
 			print("\t".join(result))
+
+			if args.split_sentence_at!=None:
+				for exp in args.split_sentence_at:
+					keyexp=exp.split("=")[0]
+					valexp=exp.split("=")[1]
+					new_sentence=False
+
+					for key,vals in zip(header,result):
+						if re.match(keyexp,key) or keyexp==key:
+
+							for val in vals.split("|")+[vals]:
+								if re.match(valexp,val) or valexp==val:
+									new_sentence=True
+									break
+						if new_sentence==True: 
+							break
+
+					if new_sentence==True: 
+						print()
+						word_nr=0
+						break
 
 	file.close()
