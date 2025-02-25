60b314ac6454a1b661fc76b16c943df747c4080d
Christian Chiarcos <christian.chiarcos@uni-a.de>
2025-02-25 10:19:23 +0100

    CoNLL export

--- gieskane.de.1650-1900/gieskane-v.0.3/jsonl2conll.py
+++ gieskane.de.1650-1900/gieskane-v.0.3/jsonl2conll.py
@@ -1,19 +1,23 @@
 import sys,os,re,json,argparse
 args=argparse.ArgumentParser(description="read JSON-L from stdin, write CoNLL; note that we don't reorder, so we expect things to be presented in reading order")
+args.add_argument("config", type=str, help="JSONL configuration for column export, note that we only export features declared there, and that multiple features can be mapped to the same column, |-concatenated")
 args.add_argument("files", nargs="*",type=str, help="JSON-L files to read from, alternatively, from stdin", default=[])
-args.add_argument("-c","--out_cols", nargs="*", type=str, help="output columns, as identified by their column label; HIGHLY RECOMMENDED: if omitted, we return columns in the order the respective feature is first observed, so, there may be different column orders for different corpora",default=[])
-args.add_argument("-c","--out_cols", nargs="*", type=str, help="output columns, as identified by their column label; HIGHLY RECOMMENDED: if omitted, we return columns in the order the respective feature is first observed, so, there may be different column orders for different corpora",default=[])
 args=args.parse_args()
+
+config=[]
+sys.stderr.write(f"reading {args.config}\n")
+with open(args.config) as input:
+	for line in input:
+		line=line.strip()
+		if not line.startswith("#") and len(line)>0:
+			config.append(json.loads(line))
 
 if len(args.files)==0: 
 	args.files=[sys.stdin]
 	sys.stderr.write("reading from stdin\n")
 
-static_cols=(len(args.out_cols)>0)
-
-if static_cols:
-	print("# "+"\t".join(args.out_cols))
-	print()
+print("# "+"\t".join([c["label"] for c in config ]))
+print()
 
 for file in args.files:
 	if isinstance(file,str):
@@ -25,11 +29,18 @@ for file in args.files:
 		line=line.strip()
 		if len(line)>0:
 			anno=json.loads(line)
-			if not static_cols:
-				for k in anno:
-					if not k in args.out_cols:
-						args.out_cols.append(k)
-			result=[ anno[k] if k in anno else "_" for k in args.out_cols ]
+			result=[]
+			for col in config:
+				add=[]
+				for key in col["keys"]:
+					if key in anno:
+						val=anno[key]
+						if not val in add: 
+							add.append(val)
+				add="|".join(add)
+				if len(add)==0:
+					add="_"
+				result.append(add)
 			print("\t".join(result))
 
 	file.close()
